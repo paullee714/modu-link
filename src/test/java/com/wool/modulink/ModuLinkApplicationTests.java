a86221eab696369b580b971babd0d12e0e2cf390
paullee714 <paul_lee@kakao.com>
2022-01-13 22:07:02 +0900

    first commit

---
+++ src/test/java/com/wool/modulink/ModuLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wool.modulink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
